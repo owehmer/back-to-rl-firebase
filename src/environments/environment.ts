f7cf2a49e15f2689ca2beada2b9d1da8396df604
owehmer <45573843+owehmer@users.noreply.github.com>
2021-08-31 20:25:07 +0200

    Firebase INIT

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDx28aaW74tKVBENhwqNZ5SINafg597Kpo',
+    authDomain: 'back-to-rl.firebaseapp.com',
+    projectId: 'back-to-rl',
+    storageBucket: 'back-to-rl.appspot.com',
+    messagingSenderId: '772596524407',
+    appId: '1:772596524407:web:92f8130db2d30434cfad72',
+    measurementId: 'G-72939333LX'
+  }
 };
 
 /*
